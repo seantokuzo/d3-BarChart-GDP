e73029987a6253930b588af77f04e84a363a4f9c
CrackerJaps <seantokuzo@gmail.com>
2022-02-20 15:57:55 -0800

    Bar chart added, fixing bugs

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        const gdpData = data.data
+        const gdpData = data.data.map(item => item[1])
+        const gdpMax = d3.max(gdpData, (d) => d)
+        const gdpMin = d3.min(gdpData, (d) => d)
         console.log(gdpData)
-        const w = 600
-        const h = 400
-        const barWidth = w / 275
-        const padding = 50
+        console.log(gdpMax)
+        console.log(gdpMin)
+        const w = 1000
+        const h = 500
+        const barWidth = (w / 275) - ((w / 275) * .2)
+        const padding = 20
 
         const xScale = d3.scaleLinear()
             .domain([0, w])
             .range([padding, w - padding])
-
         const yScale = d3.scaleLinear()
-            .domain([0, d3.max(gdpData, (d) => d[1])])
-            .range(padding, h - padding)
+            .domain([0, gdpMax])
+            .range([0, h - padding])
 
         const svg = d3.select('.svg-div')
             .append('svg')
@@ -28,10 +31,66 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
             .data(gdpData)
             .enter()
             .append('rect')
-            .attr('x', (d, i) => xScale(i * 20))
-            .attr('y', (d, i) => yScale(h - (d[1] * 3)))
-            .attr('width', barWidth)
-            .attr('height', (d, i) => yScale(d[1] * 3))
-            .attr('fill', 'red')
+            .attr('x', (d, i) => xScale(i * (w / 275)))
+            .attr('y', (d) => h - yScale(d) - padding)
+            .attr('width', xScale(barWidth) - 11)
+            .attr('height', (d) => yScale(d))
+            .attr('fill', 'grey')
+
+        const xAxis = d3.axisBottom(xScale);
+        const yAxis = d3.axisLeft(yScale);
+
+        // svg.append("g")
+        //     .attr("transform", "translate(0," + (h - padding) + ")")
+        //     .call(xAxis)
+        svg.append("g")
+            .call(xAxis)
+            .attr('id', 'x-axis')
+            .attr("transform", "translate(0," + (h - padding) + ")")
+
+        svg.append("g")
+            .call(yAxis)
+            .attr('id', 'y-axis')
+            .attr("transform", "translate(0, " + padding + ")")
+
+
+        // svg.append("rect")
+        //     .attr("x", 0)
+        //     .attr("y", 0)
+        //     .attr("height", h)
+        //     .attr("width", w)
+        //     .style("stroke", '#000000')
+        //     .style("fill", "none")
+        //     .style("stroke-width", '1px')
 
     })
+
+// const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+// const w = 500;
+// const h = 100;
+// const svg = d3.select(".svg-div")
+//     .append("svg")
+//     .attr("width", w)
+//     .attr("height", h);
+//     svg.selectAll("rect")
+//         .data(dataset)
+//         .enter()
+//         .append("rect")
+//         .attr("x", (d, i) => i * 30)
+//         .attr("y", (d, i) => h - 3 * d)
+//         .attr("width", 25)
+//         .attr("height", (d, i) => d * 3)
+//         .attr("fill", "navy")
+
+//     svg.selectAll("text")
+//         .data(dataset)
+//         .enter()
+//         .append("text")
+//         .attr("x", (d, i) => i * 30)
+//         .attr("y", (d, i) => h - 3 * d - 3)
+//         .text((d, i) => d)
+//         .style("font-size", "25px")
+//         .style("fill", "red")
+//         .attr("class", "bar")
+//         .append("title")
+//         .text((d) => d)
